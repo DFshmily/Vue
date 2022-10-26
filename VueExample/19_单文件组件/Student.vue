9b95c2c0cb0a00396be63b70667f6837e811222a
DFshmily <68329065+DFshmily@users.noreply.github.com>
2022-10-26 22:10:09 +0800

    Add files via upload

---
+++ VueExample/19_单文件组件/Student.vue
@@ -0,0 +1,29 @@
+<template>
+    <!-- //组件的结构 -->
+    <div class="demo">
+        <h2>学生姓名：{{ name }}</h2>
+        <h2>学生年龄：{{ age }}</h2>
+    </div>
+</template>
+
+<script>
+//组件交互相关的代码（数据、方法等等）
+export default {//默认暴露
+    name: 'Student',//组件名称
+    data() {
+        return {
+            name: 'DFshmily',
+            age: 20
+
+        }
+    }
+}
+
+</script>
+
+<style>
+/*组件的样式*/
+.demo {
+    background-color: orange;
+}
+</style>
---
+++ VueExample/19_单文件组件/main.js
@@ -0,0 +1,7 @@
+import App from './App.vue'
+
+new Vue({
+    el:'#root',
+    template:`<app></app>`,
+    components:{App},
+})
